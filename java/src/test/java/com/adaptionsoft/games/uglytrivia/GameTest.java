dd0ff50e8574ae819e38720f5b74e9216a9c2864
Kim Kao <yikaikao@gmail.com>
2017-02-24 19:08:40 +0800

    add the user intent test

---
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -0,0 +1,17 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kim on 2017/2/24.
+ */
+public class GameTest {
+
+    //TODO-user-intent : the game should be over if a player rolls the dice and answers each question correctly for 6 times
+
+    //TODO-user-intent : the game should be over if a player rolls the dice for 7 times and answer the question wrongly for 1 time followed by an odd rolling number but then correctly for 6 times.
+
+    //TODO-user-intent : the game should be over if a player rolls the diec for 8 times and answers the question wrongly for 1 time followed by an even rolling number but then correctly for 7 times.
+
+
+}
